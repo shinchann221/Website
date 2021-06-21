2daae0c9736effe6386e6d3abeb501e3fe84628e
Paras Jain <parasj221@gmail.com>
2021-06-21 15:55:42 +0530

    fixed es-lint errors

--- functions/mail.js
+++ functions/mail.js
@@ -1,12 +1,12 @@
-const functions = require('firebase-functions');
-const nodemailer = require('nodemailer');
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
 
 const gmailEmail = functions.config().gmail.login; //add gmail id to config
 const gmailPassword = functions.config().gmail.pass;    //add gmail password to config
 
 var goMail = function (message,email_subject) {
 
-    var outputtext = 'Hey you got a query. Details are provided below \n\n\n' + 'message: ' + message;
+    var outputtext = "Hey you got a query. Details are provided below \n\n\n" + "message: " + message;
 
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -18,20 +18,19 @@ var goMail = function (message,email_subject) {
         }
     });
     const mailOptions = {
-        to: 'neemtreeagrosolutions@gmail.com',
+        to: "neemtreeagrosolutions@gmail.com",
         from: gmailEmail,
         subject: email_subject,
         text: outputtext,
     };
 
     // eslint-disable-next-line consistent-return
-    transporter.sendMail(mailOptions, (err, info) => {
+    transporter.sendMail(mailOptions, (err) => {
         if (err) {
-            console.log(err);
-            response.json({ message: "message not sent: an error occured; check the server's console log" });
+            functions.logger.log(err);
         }
         else {
-            response.json({ message: `Thank You For Reaching Us We'll Contact You Soon` });
+            functions.logger.log("Mail Successfully Sent");
         }
     })
 }
